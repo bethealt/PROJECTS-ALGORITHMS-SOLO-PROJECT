f3cf75efb366dc3d708f5be85526e895dc9ac152
Tara Bethea <tara.bethea@outlook.com>
2021-11-05 15:43:18 -0700

    progress on the frontend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,26 +2,56 @@ import React from 'react';
 import {Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavLink, NavItem} from 'reactstrap';
 
 const NavBar = (props) => {
+    const {admin} = props;
     return (
         <Container>
             <Navbar color='faded' light>
-                <NavbarBrand className='me-auto' href='/'>
-                heartBEAT
-                </NavbarBrand>
+                <NavbarBrand className='me-auto' href='/'>heartBEAT</NavbarBrand>
                 <NavbarToggler className='me-2' onClick={function noRefCheck(){}}/>
                 <Collapse navbar>
-                <Nav navbar>
-                    <NavItem>
-                    <NavLink href='/components/'>
-                        Components
-                    </NavLink>
-                    </NavItem>
-                    <NavItem>
-                    <NavLink href='https://github.com/reactstrap/reactstrap'>
-                        GitHub
-                    </NavLink>
-                    </NavItem>
-                </Nav>
+                    {
+                        (admin === true) ?
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink href='/admin'>
+                                    Admin login
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href='/add'>
+                                    Add a Course
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href='/dashboard'>
+                                    Dashboard
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href='/catalog'>
+                                    Course Catalog
+                                </NavLink>
+                            </NavItem>
+                        </Nav> 
+                        :
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink href='/login'>
+                                    Login/Register
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href='/dashboard'>
+                                    Dashboard
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href='/catalog'>
+                                    Course Catalog
+                                </NavLink>
+                            </NavItem>
+                        </Nav> 
+                    }                           
                 </Collapse>
             </Navbar>
         </Container>
