4578b26bfcbfe57095c1ed567a3972d75dc6ce49
Tara Bethea <tara.bethea@outlook.com>
2021-11-08 13:47:49 -0800

    server updates

--- server/server.js
+++ server/server.js
@@ -3,20 +3,20 @@ const app = express();
 const cors = require('cors');
 const socket = require('socket.io');
 const cookieParser = require('cookie-parser'); 
-const mongooose = require('./config/mongoose.config');
+const jwt = require("jsonwebtoken");
+const mongoose = require('./config/mongoose.config');
 const port = 8000;
 
-//requiring the dotenv library and invoking its config function
+//requires the dotenv library and invokes its config function
 const dotenv = require('dotenv').config({ debug: process.env.DEBUG });
 const buf = Buffer.from('hello world')
 const opt = { debug: true }
 const config = dotenv.parse(buf, opt)  
 // expect a debug message because the buffer is not in KEY=VAL form
 
-const jwt = require("jsonwebtoken");
-
-
-app.use(cors());
+//enables the app to send and read cookies with each request/response
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -36,8 +36,8 @@ const payload = {
    
 const userToken = jwt.sign(payload, process.env.SECRET_KEY);
  
-// to initialize the socket, we need to invoke a new instance of socket.io and pass it our express server instance
-// We must also include a configuration settings object to prevent CORS errors
+//initializes the socket: invokes a new instance of socket.io and passes the express server instance
+//always include a configuration settings object to prevent CORS errors
 const io = require("socket.io")(server, {
     cors: {
         origin: 'http://localhost:3000',
