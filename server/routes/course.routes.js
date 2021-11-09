24da79e20f5d84bb228e09707a3732d7a0069560
Tara Bethea <tara.bethea@outlook.com>
2021-11-09 13:07:32 -0800

    additional authenticate updates

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,9 +1,10 @@
 const CourseController = require('../controllers/course.controller');
+const { authenticate } = require('../config/jwt.config');
 
 module.exports = function(app){
     app.post('/api/courses', CourseController.create);
-    app.get('/api/courses', CourseController.read);
-    app.get('/api/courses/:id', CourseController.readOne);
+    app.get('/api/courses', authenticate, CourseController.read);
+    app.get('/api/courses/:id',authenticate, CourseController.readOne);
     app.put('/api/courses/:id', CourseController.update);
     app.delete('/api/courses/:id', CourseController.delete);
 };
