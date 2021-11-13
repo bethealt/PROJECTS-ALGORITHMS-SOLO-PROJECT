86423ef2cde7c90dcbb99fda10614a1153d66d3e
Tara Bethea <tara.bethea@outlook.com>
2021-11-12 23:26:26 -0800

    socket.io updates

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,83 +1,18 @@
 import React, {useState} from 'react';
-import axios from 'axios';
 import {Container, Nav, NavItem, NavLink, TabContent, TabPane, Row, Col} from 'reactstrap';
 import classnames from 'classnames';
-import {navigate} from '@reach/router';
 
 import LoginForm from '../components/LoginForm';
 import UserForm from '../components/UserForm';
 
 const Login = (props) => {
-    const dbPort = process.env.DB_PORT
-    const { 
-        admin, 
-        errors, setErrors, 
-        users, setUsers, 
-        firstName, lastName,
-        emailAddress, birthDate,
-        zipcode, 
-        password,
-    } = props;
-    const [activeTab, setActiveTab] = useState('1');
+    const {dbHost, errors, setErrors} = props;
     const [userRegistered, setUserRegistered] = useState(false);
     const [userLoggedIn, setUserLoggedIn] = useState(false);
-    
+    const [activeTab, setActiveTab] = useState('1');
+
     const toggle = (tab) => {
         if(activeTab !== tab) setActiveTab(tab);
-    }
-
-    const registerUser = user => {
-        axios.post(`http://localhost:${dbPort}/api/users/register`, {
-            firstName,
-            lastName,
-            emailAddress,
-            birthDate,
-            zipcode,
-            password}, 
-            //ensures that cookies are sent with each request; Middleware verifies who is logged in
-            {withCredentials: true})
-        .then(res => {
-            console.log(res);
-            setUsers([...users, res.data]);
-            setUserRegistered(!userRegistered);
-            setUserLoggedIn(!userLoggedIn);
-            navigate("/dashboard");
-        })
-        .catch(err => {
-            const errorResponse = err.response.data.errors;
-            const errorArr = [];
-            for (const key of Object.keys(errorResponse)) {
-                errorArr.push(errorResponse[key].message)
-            }
-            setErrors(errorArr);
-            console.log(errorArr);
-        })
-    }
-
-    const loginUser = user => {
-        axios.post(`http://localhost:${dbPort}/api/users/login`, {
-            emailAddress, 
-            password}, 
-            {withCredentials: true})
-        .then(res => {
-            setUserLoggedIn(!userLoggedIn);
-            // eslint-disable-next-line no-lone-blocks
-            {
-                admin === true  ?
-                navigate("/admin") 
-                :
-                navigate("/dashboard")
-            };
-        })
-        .catch(err => {
-            const errorResponse = err.response.data.errors;
-            const errorArr = [];
-            for (const key of Object.keys(errorResponse)) {
-                errorArr.push(errorResponse[key].message)
-            }
-            setErrors(errorArr);
-            console.log(errorArr);
-        })
     }
 
     return(
@@ -106,8 +41,11 @@ const Login = (props) => {
                             sm="12"><br/>
                             <h4>Welcome Back!</h4><br/>
                             <LoginForm
-                                onSubmitProp = {loginUser} 
+                                dbHost={dbHost}
+                                userLoggedIn={userLoggedIn}
+                                setUserLoggedIn={setUserLoggedIn}
                                 errors={errors} 
+                                setErrors={setErrors}
                             />
                         </Col>
                     </Row>
@@ -118,9 +56,12 @@ const Login = (props) => {
                             md={{ offset: 1, size: 10}}
                             sm="12"><br/>
                             <h4>Welcome to heartBEAT!</h4><br/>
-                            <UserForm 
-                                onSubmitProp={registerUser} 
-                                errors={errors} 
+                            <UserForm
+                                dbHost={dbHost}
+                                userRegistered={userRegistered}
+                                setUserRegistered={setUserRegistered}
+                                errors={errors}
+                                setErrors={setErrors} 
                             />
                         </Col>
                     </Row>
